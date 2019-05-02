c7c670fc350187b6b55a5dd5ec581524a3af83ef
Kevin Grimm <49559740+kevinggrimm@users.noreply.github.com>
2019-05-02 08:01:02 -0400

    Update and rename 04_select_within_select_quiz to 04_select_within_select_quiz.sql
    Converted to .sql file / Updated comments

--- 04_select_within_select_quiz.sql
+++ 04_select_within_select_quiz.sql
@@ -1,19 +1,19 @@
-I. Select the code that shows the name, region and population of the smallest country in each region
+-- I. Select the code that shows the name, region and population of the smallest country in each region
 
 SELECT region, name, population FROM bbc x WHERE population <= ALL
   (SELECT population FROM bbc y WHERE y.region=x.region AND population>0)
 
-II. Select the code that shows the countries belonging to regions with all populations over 50000
+-- II. Select the code that shows the countries belonging to regions with all populations over 50000
 
 SELECT name, region, population FROM bbc x WHERE 50000 < ALL
   (SELECT population FROM bbc y WHERE x.region=y.region AND y.population>0)
 
-III. Select the code that shows the countries with a less than a third of the population of the countries around it
+-- III. Select the code that shows the countries with a less than a third of the population of the countries around it
 
 SELECT name, region FROM bbc x 
   WHERE population < ALL (SELECT population/3 FROM bbc y WHERE y.region=x.region AND y.name != x.name)
 
-IV. Select the result that would be obtained from the following code:
+-- IV. Select the result that would be obtained from the following code:
 
 SELECT name FROM bbc
   WHERE population > 
@@ -27,18 +27,18 @@ Germany
 Russia
 Turkey
 
-V. Select the code that would show the countries with a greater GDP than any country in Africa (some countries may have NULL gdp values).
+-- V. Select the code that would show the countries with a greater GDP than any country in Africa (some countries may have NULL gdp values).
 
 SELECT name FROM bbc
   WHERE gdp > (SELECT MAX(gdp) FROM bbc WHERE region = 'Africa')
 
-VI. Select the code that shows the countries with population smaller than Russia but bigger than Denmark
+-- VI. Select the code that shows the countries with population smaller than Russia but bigger than Denmark
 
 SELECT name FROM bbc
   WHERE population < (SELECT population FROM bbc WHERE name='Russia')
     AND population > (SELECT population FROM bbc WHERE name='Denmark')
 
-VII. Select the result that would be obtained from the following code:
+-- VII. Select the result that would be obtained from the following code:
 
 SELECT name FROM bbc
   WHERE population > ALL
