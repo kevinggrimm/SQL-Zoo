f26fedc51980811a4b5840a8615f7ffb42c867a2
Kevin Grimm <49559740+kevinggrimm@users.noreply.github.com>
2019-05-02 07:56:26 -0400

    Update 01_select_basics_quiz.sql
    Converted to sql file / Updated comments

--- 01_select_basics_quiz.sql
+++ 01_select_basics_quiz.sql
@@ -1,15 +1,15 @@
-I. Select the code which produces this table
+-- I. Select the code which produces this table
 
-name	      population
-Bahrain	    1234571
-Swaziland	  1220000
-Timor-Leste	1066409
+name	        population
+Bahrain	        1234571
+Swaziland	    1220000
+Timor-Leste	    1066409
 
 SELECT name, population
     FROM world
   WHERE population BETWEEN 1000000 AND 1250000
 
-II. Pick the result you would obtain from this code:
+-- II. Pick the result you would obtain from this code:
 
 SELECT name, population
 FROM world
@@ -18,7 +18,7 @@ WHERE name LIKE 'Al%'
 Albania	3200000
 Algeria	32900000
 
-III. Select the code which shows the countries that end in A or L
+-- III. Select the code which shows the countries that end in A or L
 
 SELECT name FROM world
   WHERE name LIKE '%a' OR name LIKE '%l'
@@ -34,7 +34,7 @@ Italy	5
 Malta	5
 Spain	5
 
-V. Here are the first few rows of the world table:
+-- V. Here are the first few rows of the world table:
 
 name	      region	    area	  population	gdp
 Afghanistan	South Asia	652225	26000000	
@@ -42,19 +42,19 @@ Albania	    Europe	    28728	  3200000	    6656000000
 Algeria	    Middle East	2400000	32900000	  75012000000
 Andorra	    Europe	    468	    64000	
 
-Pick the result you would obtain from this code:
+-- Pick the result you would obtain from this code:
 
 SELECT name, area*2 FROM world WHERE population = 64000
 
 Andorra	936
 
-VI. Select the code that would show the countries with an area larger than 50000 and a population smaller than 10000000
+-- VI. Select the code that would show the countries with an area larger than 50000 and a population smaller than 10000000
 
 SELECT name, area, population
     FROM world
   WHERE area > 50000 and population < 10000000
 
-VII. Select the code that shows the population density of China, Australia, Nigeria and France
+-- VII. Select the code that shows the population density of China, Australia, Nigeria and France
 
 SELECT name, population/area
     FROM world
